6624f2d4ab5b393f5988df0b7c1d76ac97064559
MinakshiMittal <49836499+MinakshiMittal@users.noreply.github.com>
2024-09-08 22:55:52 +0530

    href in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,14 +5,13 @@ export const Sidebar = () => {
 
   return (
     <div className="lg:w-1/12 w-full bg-white flex lg:flex-col lg:justify-start  lg:items-center items-center border-r shadow-md lg:px-0 px-2 py-4 gap-4 lg:gap-0 lg:space-y-2 font-semibold text-sm text-purple-600">
-      <a className="cursor-pointer" onClick={() => navigate("/")}>
+      <a className="cursor-pointer" href='/'>
         Contacts
       </a>
 
       <a
         className="cursor-pointer"
-        onClick={() => navigate("/maps-and-charts")}
-      >
+        href='/maps-and-charts' >
         Maps & Charts
       </a>
     </div>
